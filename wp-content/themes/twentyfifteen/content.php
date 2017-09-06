4806203823db4971008ffbd130431a73ce0844ee
Lino Espinoza <lino.espinoza@entel.pe>
2017-09-05 19:49:30 -0500

    Clase 5/09 Template tags

--- wp-content/themes/twentyfifteen/content.php
+++ wp-content/themes/twentyfifteen/content.php
@@ -20,6 +20,7 @@
 		<?php
 			if ( is_single() ) :
 				the_title( '<h1 class="entry-title">', '</h1>' );
+				//the_excerpt();
 			else :
 				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			endif;
